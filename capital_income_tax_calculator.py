e8a759caa5f213073e4c3460f44fa8e15e9195b3
Peter Todorov <peter.todorov@live.com>
2021-05-30 20:57:31 +0300

    Reduce previous years losses from earnings

--- capital_income_tax_calculator.py
+++ capital_income_tax_calculator.py
@@ -1,6 +1,5 @@
 import csv
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -9,6 +8,7 @@ BASE_TAX_PERCENTAGE = 30
 MARGIN_TAX_PERCENTAGE = 34
 MARGIN_TAX_THRESHOLD = 50000
 ZERO_TAX_THRESHOLD = 1000
+N_YEARS_LOSSES_ACCUMULATED = 5
 NAMING_CONVERSION = {  # Define here the headers in CSV file
     "sell_date": "Luovutusaika",
     "profit": "Voitto tai tappio EUR",
@@ -17,16 +17,53 @@ NAMING_CONVERSION = {  # Define here the headers in CSV file
 }
 
 
-def taxAmount(profit):
-    if profit > MARGIN_TAX_THRESHOLD:
+def taxAmount(earnings):
+    # Calculate earning based on previous years losses
+    previous_years_earnings = sum(earnings[-N_YEARS_LOSSES_ACCUMULATED:-1])
+    earning = earnings[-1]
+    if previous_years_earnings < 0:
+        earning += previous_years_earnings
+
+    # Calculate tax for earning
+    if earning > MARGIN_TAX_THRESHOLD:
         base_tax = BASE_TAX_PERCENTAGE * MARGIN_TAX_THRESHOLD
-        margin_tax = MARGIN_TAX_PERCENTAGE * (profit - MARGIN_TAX_THRESHOLD)
+        margin_tax = MARGIN_TAX_PERCENTAGE * (earning - MARGIN_TAX_THRESHOLD)
         tax = (base_tax + margin_tax) / 100
-    elif profit <= ZERO_TAX_THRESHOLD:
+    elif earning <= ZERO_TAX_THRESHOLD:
         tax = 0
     else:
-        tax = BASE_TAX_PERCENTAGE * profit / 100
+        tax = BASE_TAX_PERCENTAGE * earning / 100
     return tax
+
+
+def printTable(texts, values):
+    # Compute header length
+    text_lengths = []
+    for i, text in enumerate(texts):
+        text_lengths.append(5 if i == 0 else max(9, len(text)))
+    table_length = sum(text_lengths) + 4 + 3 * max(0, len(text_lengths) - 1)
+
+    # Print header
+    print(table_length * "-")
+    print("| ", end="")
+    for i, text in enumerate(texts):
+        number_of_spaces = max(0, text_lengths[i] - len(text))
+        print("{}{}".format(" " * number_of_spaces, text), end=" | ")
+
+    # Print table
+    print("\n" + table_length * "-")
+    for i in range(len(values[0])):
+        if i == len(values[0]) - 1:
+            print(table_length * "-")
+        print("| ", end="")
+        for j in range(len(values)):
+            value = values[j][i]
+            if type(value) == np.float64:
+                value = "{0:.2f}".format(value)
+            number_of_spaces = max(0, text_lengths[j] - len(str(value)))
+            print("{}{}".format(" " * number_of_spaces, value), end=" | ")
+        print()
+    print(table_length * "-")
 
 
 def main():
@@ -35,10 +72,10 @@ def main():
     # Read CSV
     csv_file = pd.read_csv(
         CSV_FILE_NAME, header=0, sep="\t", encoding="utf-16", decimal=",")
-    csv_file["Luovutusaika"] = pd.to_datetime(
-        csv_file["Luovutusaika"], format="%d.%m.%Y")
-    min_year = min(csv_file["Luovutusaika"]).year
-    max_year = max(csv_file["Luovutusaika"]).year
+    csv_file[nc["sell_date"]] = pd.to_datetime(
+        csv_file[nc["sell_date"]], format="%d.%m.%Y")
+    min_year = min(csv_file[nc["sell_date"]]).year
+    max_year = max(csv_file[nc["sell_date"]]).year
 
     # Process annual info
     years = list(range(min_year, max_year + 1))
@@ -54,18 +91,18 @@ def main():
             (csv_file[nc["sell_date"]] > f"01-01-{year}") &
             (csv_file[nc["sell_date"]] < f"31-12-{year}")
         ]
-        loss = rows[rows[nc["profit"]] < 0][nc["profit"]].to_numpy().sum()
+        loss = abs(rows[rows[nc["profit"]] < 0][nc["profit"]].to_numpy().sum())
         profit = rows[rows[nc["profit"]] >= 0][nc["profit"]].to_numpy().sum()
         buy_cost = rows[nc["buy_cost"]].astype(float).to_numpy().sum()
         sell_cost = rows[nc["sell_cost"]].astype(float).to_numpy().sum()
-        earning = profit - buy_cost - sell_cost + loss
-        tax = taxAmount(earning)
+        earning = profit - buy_cost - sell_cost - loss
+        earnings.append(earning)
+        tax = taxAmount(earnings)
         net_earning = earning - tax
         buy_costs.append(buy_cost)
         sell_costs.append(sell_cost)
         profits.append(profit)
         losses.append(loss)
-        earnings.append(earning)
         taxes.append(tax)
         net_earnings.append(net_earning)
 
@@ -92,28 +129,7 @@ def main():
     ]
     for i in range(1, len(values)):
         values[i].append(sum(values[i]))
-    text_lengths = []
-    for i, text in enumerate(texts):
-        text_lengths.append(5 if i == 0 else max(9, len(text)))
-    table_length = sum(text_lengths) + 4 + 3 * max(0, len(text_lengths) - 1)
-    print(table_length * "-")
-    print("| ", end="")
-    for i, text in enumerate(texts):
-        number_of_spaces = max(0, text_lengths[i] - len(text))
-        print("{}{}".format(" " * number_of_spaces, text), end=" | ")
-    print("\n" + table_length * "-")
-    for i in range(len(taxes)):
-        if i == len(taxes) - 1:
-            print(table_length * "-")
-        print("| ", end="")
-        for j in range(len(values)):
-            value = values[j][i]
-            if type(value) == np.float64:
-                value = "{0:.2f}".format(value)
-            number_of_spaces = max(0, text_lengths[j] - len(str(value)))
-            print("{}{}".format(" " * number_of_spaces, value), end=" | ")
-        print()
-    print(table_length * "-")
+    printTable(texts, values)
 
 
 main()

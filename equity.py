47b4387023b95c0ebd778701192e71eb164b456a
Peter Todorov <peter.todorov@live.com>
2022-07-29 13:06:26 +0200

    Change student loan name

--- equity.py
+++ equity.py
@@ -6,14 +6,14 @@ with open("finance_info.yaml") as file:
 PORTFOLIO_BALANCE = int(sys.argv[1])
 CASH = int(sys.argv[2])
 PORTFOLIO_INVESTED = yaml_file["PORTFOLIO_INVESTED"]
-LOAN = yaml_file["ORIGINAL_LOAN"]
+LOAN = yaml_file["STUDY_LOAN_LEFT"]
 LOAN_BENEFIT = yaml_file["LOAN_BENEFIT"]
 STOCK_TAX = yaml_file["STOCK_TAX"]
 DIVIDEND_TAX = yaml_file["DIVIDEND_TAX"]
 
 print(
     "Savings:           ",
-    PORTFOLIO_INVESTED - LOAN + CASH + STOCK_TAX)
+    PORTFOLIO_INVESTED + CASH + STOCK_TAX - LOAN - LOAN_BENEFIT)
 print(
     "Investment profits:",
     PORTFOLIO_BALANCE - PORTFOLIO_INVESTED + LOAN_BENEFIT - STOCK_TAX)
